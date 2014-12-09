bc154080ba0e403eca82b79661222f073e159960
Roman <romanyu827@gmail.com>
2014-12-09 17:52:33 +0800

    implement images array

--- CollectionViewTesting/CollectionViewTesting/MyCollectionViewController.m
+++ CollectionViewTesting/CollectionViewTesting/MyCollectionViewController.m
@@ -10,14 +10,18 @@
 
 @interface MyCollectionViewController ()
 
+@property(nonatomic) NSArray *images;
 @end
 
 @implementation MyCollectionViewController
+@synthesize images;
 
 static NSString * const reuseIdentifier = @"MyCell";
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    self.images = @[@"geek", @"geek2", @"geek3", @"geek4", @"geek5"];
     
     // Uncomment the following line to preserve selection between presentations
     // self.clearsSelectionOnViewWillAppear = NO;
@@ -53,14 +57,14 @@ static NSString * const reuseIdentifier = @"MyCell";
 
 - (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section {
 #warning Incomplete method implementation -- Return the number of items in the section
-    return 1;
+    return [self.images count];
 }
 
 - (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath {
     MyCollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:reuseIdentifier forIndexPath:indexPath];
     
     // Configure the cell
-    [cell.myImageView setImage:[UIImage imageNamed:@"geek"]];
+    [cell.myImageView setImage:[UIImage imageNamed:[self.images objectAtIndex:indexPath.item]]];
     
     return cell;
 }
